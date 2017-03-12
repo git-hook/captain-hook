3563cc0d38b0f30c468b32f5edd6661a0b390aa5
Eric Crosson <esc@ericcrosson.com>
2017-03-12 03:58:38 -0500

    Document functions

--- captain-hook.sh
+++ captain-hook.sh
@@ -31,6 +31,7 @@ debug() {
     fi
 }
 
+# Return the sorted list of files comprising a git-hook pipeline.
 _getPipeline() {
     searchDir=$1; shift
     find "${searchDir}" \
@@ -38,10 +39,13 @@ _getPipeline() {
         | sort
 }
 
+# Test if the supplied script is classified as a "ensured" script.
 _ensuredScript() {
     [ -z "$(echo "${script}" | sed -ne '/[0-9]\{3\}+/p')" ]
 }
 
+# Given the existing exit-code ($1) and the exit-code of the last
+# script that ran ($2), return the new exit-code for this pipeline.
 _updateRc() {
     if [[ $1 -eq 0 ]]; then
         echo "$2"
@@ -50,11 +54,17 @@ _updateRc() {
     fi
 }
 
+# Test if this pipeline is in short-circuit mode, given the current
+# pipeline's exit-code.
 _shortCircuit() {
     local -r rc=$1; shift
     [ "${rc}" -le 0 ]
 }
 
+# Determine the exit code for the entire pipeline.
+#
+# Note: this function should be called at the end of a pipeline's
+# execution.
 _determineExitCode() {
     local -r rc=$1; shift
     # negative exit codes indicate a short-circuit without failure
@@ -65,6 +75,8 @@ _determineExitCode() {
     fi
 }
 
+# This method is called by captain-hook.  It will run the user's
+# defined git-hook pipeline when a client-side hook is invoked by git.
 runHook() {
     local -r hook=$1; shift
     local rc=0
