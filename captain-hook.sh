f58b6725ff09d92258fe6f50f748a9ac317e8385
Eric Crosson <esc@ericcrosson.com>
2017-03-12 01:52:04 -0600

    Add more passing tests

--- captain-hook.sh
+++ captain-hook.sh
@@ -19,28 +19,35 @@
 #
 # Note: may not work great with the in-place test status updates.
 #       Consider logging with `tee` to a file for inspection.
-readonly _debug=0
+declare _debug=0
 
 # Use this command instead of `echo` to only print information when
 # debug-mode is enabled.
 debug() {
-    if [[ ${_debug} -gt 0 ]]; then
+    if [[ "${_debug}" -gt 0 ]]; then
         >&2 echo "$@"
     fi
+}
+
+_getPipeline() {
+    searchDir=$1; shift
+    find "${searchDir}" \
+         -regex ".*/[0-9]\{3\}+\{0,1\}.*" \
+        | sort
 }
 
 runHook() {
     local -r hook=$1; shift
     local rc=0
 
-    for script in $(find "${hook}.d" \
-                         -regextype sed \
+    for script in "$(find "${hook}.d" \
+                           -regextype sed \
                          -regex ".*/[0-9]\{3\}+\{0,1\}.*" | \
-                        sort); do
+                        sort)"; do
 
         # if part of the hook has failed, skip forward to the
         # `ensured` scripts
-        if [[ ${rc} -ne 0 && \
+        if [[ "${rc}" -ne 0 && \
                   -z "$(echo "${script}" | sed -ne '/[0-9]\{3\}+/p')" ]]
         then
             continue
@@ -48,20 +55,20 @@ runHook() {
 
         chmod u+x "${script}"
         debug "Executing ${script}"
-        ${script} "$@"
+        "${script}" "$@"
         local script_rc=$?
         # exit code can only be modified one time
-        if [[ ${rc} -eq 0 ]]; then
-            rc=${script_rc}
+        if [[ "${rc}" -eq 0 ]]; then
+            rc="${script_rc}"
         fi
     done
 
     # negative exit codes indicate a short-circuit without failure
     debug "Return code is ${rc}"
-    if [[ ${rc} -le 0 ]]; then
+    if [[ "${rc}" -le 0 ]]; then
         exit 0
     else
-        exit ${rc}
+        exit "${rc}"
     fi
 }
 export runHook
--- test/test-captain-hook.sh
+++ test/test-captain-hook.sh
@@ -1,17 +1,25 @@
-#!/usr/bin/env bats
+#!./test/libs/bats/bin/bats
 # Written by Eric Crosson
 # 2017-03-12
 #
-# Test captain-hook.sh
+# Test debug method of captain-hook.sh.
+
+load 'libs/bats-support/load'
+load 'libs/bats-assert/load'
 
 setup() {
     . "${BATS_TEST_DIRNAME}/../captain-hook.sh"
+    TMPDIR=$(mktemp -d bats.XXXX)
+}
+
+teardown() {
+    [ -d "${TMPDIR}" ] && rm -rf "${TMPDIR}"
 }
 
 @test "debug should print nothing when debug mode is not enabled" {
     run debug 'A light switch is also a dark switch.'
-    [ "${status}" -eq 0 ]
-    [ "${output}" = '' ]
+    assert [ "${status}" -eq 0 ]
+    assert [ "${output}" = '' ]
 }
 
 @test "debug mode should print all args when debug mode is enabled" {
@@ -19,6 +27,50 @@ setup() {
     debug "Nope"
     truth='When you clean a vacuum cleaner, you are the vacuum cleaner.'
     run debug "${truth}"
-    [ "${status}" -eq 0 ]
-    [ "${output}" = "${truth}" ]
+    assert [ "${status}" -eq 0 ]
+    assert [ "${output}" = "${truth}" ]
+}
+
+generateRandomValidHookPipeline() {
+    prefix="$1"; shift
+    hooks="${prefix}/hooks"
+    mkdir -p "${hooks}"
+    generateRandomValidHookScripts "${hooks}"
+}
+
+generateRandomValidHookScripts() {
+    outputDir="${1:-${TMPDIR}}"
+    touch "${outputDir}/$(( RANDOM % 10))$(( RANDOM % 10))$(( RANDOM % 10))-generated-test.sh"
+}
+
+generateRandomInvalidHookPipeline() {
+    prefix="$1"; shift
+    hooks="${prefix}/hooks"
+    mkdir -p "${hooks}"
+    generateRandomInvalidHookScripts "${hooks}"
+}
+
+generateRandomInvalidHookScripts() {
+    outputDir="${1:-${TMPDIR}}"
+    # random number between 5 and 10
+    numScripts=$(( ( RANDOM % 5 ) + 5 ))
+    for i in $(seq 1 "${numScripts}"); do
+        touch "${outputDir}/NaN-super-test-should-be-ignored.sh"
+        touch "${outputDir}/NaN-123-test-should-be-ignored.sh"
+        touch "${outputDir}/NaN-123+-test-should-be-ignored.sh"
+    done
+}
+
+@test "_getPipeline should return files in numerical order" {
+    generateRandomValidHookPipeline "${TMPDIR}"
+    run _getPipeline "${TMPDIR}"
+    assert [ "${status}" -eq 0 ]
+    assert [ "$(echo ${output} | grep -o '[0-9][0-9][0-9]')" = "$(echo ${output} | grep -o '[0-9][0-9][0-9]' | sort -u)" ]
+}
+
+@test "_getPipeline should ignore files not beginning with a three-digit number" {
+    generateRandomInvalidHookPipeline "${TMPDIR}"
+    run _getPipeline "${TMPDIR}"
+    assert [ "${status}" -eq 0 ]
+    assert [ "${output}" = '' ]
 }
